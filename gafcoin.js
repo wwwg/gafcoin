35592c60783f07419362f9395a8c4b232cc8c5f2
wwwg <xxperipsis@gmail.com>
2018-03-16 21:09:48 -0500

    change test method

--- gafcoin.js
+++ gafcoin.js
@@ -1,7 +1,6 @@
 // constants (may make configurable later)
 const P2P_PORT = 9284,
-    P2P_TEST_PORT = 9285, // to run 2 nodes on one computer
-    IS_TEST = !!(process.env['GAF_TEST']);
+    P2P_TEST_PORT = 9285; // to run 2 nodes on one computer
 
 // imports
 const crypto = require('crypto'),
@@ -40,13 +39,4 @@ class NetNode {
             console.log(ws._socket);
         });
     }
-}
-
-let nnode;
-if (IS_TEST) {
-    console.log('im a test node yay');
-    nnode = new NetNode(P2P_TEST_PORT);
-} else {
-    console.log('im not a test not yay');
-    nnode = new NetNode(P2P_PORT);
 }
--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 # i am too lazy to execute 2 lines of bash
 clear
-GAF_TEST=1 node gafcoin.js
+node gafcoin.js
